8b29ebb0f12c02150fe22e451a86effee88e8c59
HarshGupta <harshgupta88911@gmail.com>
2025-01-17 19:24:38 +0530

    Initial commit :rocket:

--- pixelmind/next.config.mjs
+++ pixelmind/next.config.mjs
@@ -3,11 +3,6 @@ const nextConfig = {
   images: {
     domains: ["static.remove.bg", "sb.kaleidousercontent.com"],
   },
-  experimental: {
-    appDir: true,
-  },
-  // If you're using the src folder structure
-  srcDir: "src",
 };
 
 export default nextConfig;
